adffa124b586f2ffd3c11a905de23e53af8f2cab
Josh Bayley <josh.g.bayley@gmail.com>
2016-05-18 16:34:05 +0100

    added editor entry point, you need to add argument to run config (g or e)

--- src/main/java/keyboard_warrior/Main.java
+++ src/main/java/keyboard_warrior/Main.java
@@ -1,14 +1,38 @@
 package keyboard_warrior;
 
+import keyboard_warrior.editor.Editor;
 import keyboard_warrior.game.Game;
 
 /**
- * The main entry point for the game.
+ * The main entry point for the game/editor.
  */
 public class Main {
 
     public static void main (String[] args)
     {
-        new Game();
+        if (args.length == 0)
+        {
+            usageMessage();
+            System.exit(0);
+        }
+
+        if (args[0].equalsIgnoreCase("g"))
+        {
+            new Game();
+        }
+        else if (args[0].equalsIgnoreCase("e"))
+        {
+            new Editor();
+        }
+        else
+        {
+            usageMessage();
+            System.exit(0);
+        }
+    }
+
+    public static void usageMessage()
+    {
+        System.out.println("Bad usage, use one of the following arguments \n\n g - Launch Game \n e - Launch Editor");
     }
 }
---
+++ src/main/java/keyboard_warrior/editor/Editor.java
@@ -0,0 +1,10 @@
+package keyboard_warrior.editor;
+
+/**
+ * Created by bayle on 17/05/2016.
+ */
+public class Editor {
+
+
+
+}
